5da1bd91bf231be0e6fc4471bce566bbb4181aaa
Mahadevan Reji <rmahadevan574@gmail.com>
2025-04-02 18:22:11 +0530

    Implement user caching in auth middleware to optimize token verification

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,6 +1,8 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
+const UserMap = new Map();
+
 exports.protect = async (req, res, next) => {
   let token;
 
@@ -11,7 +13,15 @@ exports.protect = async (req, res, next) => {
     try {
       token = req.headers.authorization.split(" ")[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      // Check if the token is already in the map
+      const cachedUser = UserMap.get(decoded.id);
+      if (cachedUser) {
+        req.user = cachedUser;
+        return next();
+      }
       req.user = await User.findById(decoded.id).select("-password");
+      // Cache the user in the map
+      UserMap.set(decoded.id, req.user);
       next();
     } catch (error) {
       res.status(401).json({ message: "Not authorized: " + error.message });
